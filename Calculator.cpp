61e4f3cf7030e4af3b808146c19774487f068d62
Christopher <bcsmithboys1@gmail.com>
2022-01-27 15:58:56 -0600

    made it actually calculate and fixed divide by zero error

--- Calculator.cpp
+++ Calculator.cpp
@@ -8,8 +8,29 @@ using namespace std;
 
 int main()
 {
+	double x = 0.0;
+	double y = 0.0;
+	double result = 0.0;
+	char oper = '+';
+
 	cout << "Calculator Console App\n\n";
 	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b |\n";
+	
+	Calculator c;
+	while (true)
+	{
+		cin >> x >> oper >> y;
+		if (oper == '/' && y == 0)
+		{
+			cout << "Dividing by 0 is wrong\n";
+			continue;
+		}
+		else
+		{
+			result = c.Calculate(x, oper, y);
+		}
+		cout << "Result is: " << result << endl;
+	}
 	return 0;
 }
 
